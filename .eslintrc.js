ae4f4a928cea47794f4711d87b421b50c0451045
zhangruilin <1130175323@qq.com>
2020-02-08 13:54:20 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,50 @@
-module.exports = {
-	root: true,
-	parser: 'babel-eslint',
-	extends: ['standard', 'plugin:react/recommended'],
-	parserOptions: {
-		ecmaVersion: 6,
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-			experimentalObjectRestSpread: true,
-			impliedStrict: true
-		}
+module.exports ={
+	"parser": "babel-eslint",
+	"env": {
+	  "browser": true,
+	  "es6": true,
+	  "node": true
 	},
-	env: {
-		browser: true,
-		node: true,
-		es6: true
+	"parserOptions": {
+	  "ecmaVersion": 6,
+	  "sourceType": "module"
 	},
-	settings: {
-		react: {
-			version: '16.2.0'
-		}
-	},
-	plugins: ['import', 'react'],
-	rules: {
-		'no-console': ['error', {allow: ['warn', 'error']}],    // 禁止conosle 允许console.warn, console.error
-		'no-alert': 'error',                                      // 禁止alert, promp, confirm
-		'no-empty': ['error', {allowEmptyCatch: true}],         // 禁止空代码块(catch除外)
-		'no-extra-semi': 'error',                                 // 禁止不必要的分号
-		'default-case': 'error',                                  // switch语句必须有default
-		'dot-notation': 'error',                                  // 尽可能使用点号
-		'no-else-return': 'error',                                // 禁止 if 语句中 return 语句之后有 else 块
-		'no-empty-function': 'error',                             // 禁止出现空函数
-		'radix': 'error',                                         // 强制在parseInt()使用基数参数
-		'semi': ['error', 'always'],
-		'quotes': ['error', 'single'],
-		'indent': ['error', 'tab', { "SwitchCase": 1 } ],
-		'no-tabs': 'off',
-		'one-var': 'off',
-		'no-mixed-spaces-and-tabs': ['off', 'smart-tabs'],
-		'no-extra-parens': 'warn',
-		'no-template-curly-in-string': 'off',
-		'key-spacing': ['warn', {beforeColon: false, 'afterColon': true}],
-		'keyword-spacing': ['warn', {before: true, after: true}],
-		'no-multi-spaces': ['warn', {ignoreEOLComments: true}],
-		'spaced-comment': 'off',
-		'comma-dangle': 'off',
-		'react/display-name': 'off',
-		'react/prop-types': ['warn', { ignore: ['match', 'location', 'history', '$$context'] }],
-		'react/self-closing-comp': ['error', {component: true}],
-		'import/no-webpack-loader-syntax': 'off'
+	"rules": {
+	  "space-in-parens": [0],
+	  "no-tabs": [0],
+	  "global-require": [0],
+	  "react/jsx-filename-extension": [0],
+	  "indent": ["error", 2],
+	  "react/jsx-indent": [0],
+	  "jsx-a11y/anchor-is-valid": [0],
+	  "import/extensions": [0],
+	  "react/jsx-curly-spacing": [0],
+	  "react/jsx-tag-spacing": [0],
+	  "react/require-default-props": [0],
+	  "class-methods-use-this": ["off"],
+	  "arrow-body-style": ["warn", "always"],
+	  "padded-blocks": ["off"],
+	  "no-trailing-spaces": ["error", { "ignoreComments": true }],
+	  "react/jsx-indent-props": [0],
+	  "no-nested-ternary": ["error"],
+	  "no-underscore-dangle": ["off", "always"],
+	  "prefer-destructuring": ["warn"],
+	  "camelcase": ["warn"],
+	  "no-irregular-whitespace": ["warn"],
+	  "jsx-a11y/no-static-element-interactions": [0],
+	  "jsx-a11y/click-events-have-key-events": [0],
+	  "no-return-assign": ["off"],
+	  "consistent-return": ["error", { "treatUndefinedAsUnspecified": true }],
+	  "dot-notation": ["warn"],
+	  "linebreak-style": "off",
+	  "quotes": ["error", "double"],
+	  "no-console": ["warn", { "allow": ["warn", "error", "log"] }],
+	  "import/no-useless-path-segments": "off",
+	  "object-curly-newline": "off",
+	  "no-shadow": "off",
+	  "react/jsx-one-expression-per-line": "off",
+	  "jsx-a11y/label-has-associated-control": "off",
+	  "jsx-a11y/label-has-for": "off",
+	  "max-len": ["error", { "code": 100 }]
 	}
-}
+  }
